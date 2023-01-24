97a4355fc187adbae0149728651103f196ff6246
L-Dragon5 <lildragon555@gmail.com>
2023-01-24 10:12:45 -0500

    Made start screen cleaner. Fixed formula for board generation for all sizes. Added localStorage browser saving. Added default text values.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { DeleteIcon } from '@chakra-ui/icons';
 import {
   Box,
   Button,
   ChakraProvider,
   Container,
   Fade,
+  Flex,
   FormControl,
+  FormHelperText,
   FormLabel,
   Heading,
+  IconButton,
   Input,
   ListItem,
   SimpleGrid,
@@ -14,21 +18,27 @@ import {
   Text,
   UnorderedList,
   useDisclosure,
+  useToast,
   VStack,
-} from "@chakra-ui/react";
-import React, { useRef, useState } from "react";
+} from '@chakra-ui/react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function App() {
-  const [spaceInput, setSpaceInput] = useState("");
-  const [allSpaces, setAllSpaces] = useState([]);
+  const [spaceInput, setSpaceInput] = useState('');
+  const [allSpaces, setAllSpaces] = useState(
+    localStorage.getItem('allSpaces')
+      ? JSON.parse(localStorage.getItem('allSpaces'))
+      : [],
+  );
   const [startGame, setStartGame] = useState(false);
   const [splitValue, setSplitValue] = useState(0);
-  const [gameName, setGameName] = useState("");
+  const [gameName, setGameName] = useState('');
   const [boardRoll, setBoardRoll] = useState(0);
   const [boardPosition, setBoardPosition] = useState(0);
 
   const formRef = useRef();
 
+  const toast = useToast();
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   function randomIntFromInterval(min, max) {
@@ -41,11 +51,32 @@ function App() {
     formRef.current.reset();
   }
 
-  function handleSubmit(e) {
+  function handleSpaceDelete(findIndex) {
+    setAllSpaces((prevState) =>
+      prevState.filter((value, index) => index !== findIndex),
+    );
+  }
+
+  function handleGameStart(e) {
     e.preventDefault();
 
-    if (allSpaces.length % 4 !== 0) {
-      alert("Total board spaces needs to be divisible by 4");
+    if (allSpaces.length < 1) {
+      toast({
+        title: 'Game Start Error',
+        description: 'Need board spaces to start',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    } else if (allSpaces.length % 4 !== 0) {
+      toast({
+        title: 'Game Start Error',
+        description: 'Total board spaces needs to be divisible by 4',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
       return;
     }
 
@@ -67,7 +98,7 @@ function App() {
 
   function handleRoll(e) {
     e.preventDefault();
-    const value = randomIntFromInterval(1, 6);
+    const value = randomIntFromInterval(1, allSpaces.length <= 12 ? 3 : 6);
     setBoardRoll(value);
     setBoardPosition((prevState) => (prevState + value) % allSpaces.length);
     setTimeout(() => {
@@ -76,11 +107,17 @@ function App() {
     onOpen();
   }
 
+  useEffect(() => {
+    if (!startGame) {
+      localStorage.setItem('allSpaces', JSON.stringify(allSpaces));
+    }
+  }, [allSpaces]);
+
   return (
     <ChakraProvider resetCSS>
-      <Box overflow="hidden">
+      <Flex overflow="hidden" h="100vh" w="100vw" alignItems="center" background="blue.100">
         {startGame ? (
-          <Box background="orange.100" h="100vh" w="100vw">
+          <Box background="orange.100" h="full" width="full">
             <VStack
               position="absolute"
               w={((splitValue + 2) * 150) / 2}
@@ -95,13 +132,17 @@ function App() {
             </VStack>
             {allSpaces.map((space, index) => {
               let background;
-              if (index === boardPosition) {
-                background = "pink.200";
-              } else if (index % (splitValue + 1) === 0) {
-                background = "orange.300";
+              let fontWeight = 'normal';
+              if (index % (splitValue + 1) === 0) {
+                background = 'orange.300';
+                fontWeight = 'bold';
               } else {
-                background = "teal.300";
+                background = 'teal.300';
               }
+              if (index === boardPosition) {
+                background = 'pink.200';
+              } 
+
               if (index < splitValue + 2) {
                 // Move Right
                 return (
@@ -112,6 +153,8 @@ function App() {
                     position="absolute"
                     left={index * 150}
                     userSelect="none"
+                    textAlign="center"
+                    fontWeight={fontWeight}
                   >
                     {space}
                   </Square>
@@ -127,6 +170,8 @@ function App() {
                     left={150 * (splitValue + 1)}
                     top={150 * (index - (splitValue + 1))}
                     userSelect="none"
+                    textAlign="center"
+                    fontWeight={fontWeight}
                   >
                     {space}
                   </Square>
@@ -139,9 +184,11 @@ function App() {
                     background={background}
                     size="150px"
                     position="absolute"
-                    left={150 * splitValue - 150 * (index - 3 * splitValue)}
+                    left={150 * (splitValue * 3 + 3) - (index * 150)}
                     top={150 * (splitValue + 1)}
                     userSelect="none"
+                    textAlign="center"
+                    fontWeight={fontWeight}
                   >
                     {space}
                   </Square>
@@ -155,8 +202,10 @@ function App() {
                     size="150px"
                     position="absolute"
                     left={0}
-                    top={150 * splitValue - 150 * (index - 4 * splitValue - 1)}
+                    top={150 * (splitValue * 4 + 4) - (index * 150)}
                     userSelect="none"
+                    textAlign="center"
+                    fontWeight={fontWeight}
                   >
                     {space}
                   </Square>
@@ -165,24 +214,36 @@ function App() {
             })}
           </Box>
         ) : (
-          <Container maxW="container.lg">
+          <Container background="white" maxW="container.lg" p={6} boxShadow="lg">
+            <Heading textAlign="center" mb={6}>Punishment Board Layout Generator</Heading>
             <SimpleGrid columns={2} spacing={10}>
               <VStack ref={formRef} as="form" onSubmit={handleSpaceAdd}>
                 <FormControl id="space-name">
                   <FormLabel>Enter Board Space Name</FormLabel>
                   <Input onChange={(e) => setSpaceInput(e.target.value)} />
+                  <FormHelperText>
+                    Place in order from top left going clockwise
+                  </FormHelperText>
                 </FormControl>
                 <Button type="submit" colorScheme="orange">
                   Add Space
                 </Button>
                 <UnorderedList>
-                  {allSpaces.map((space) => (
-                    <ListItem key={space}>{space}</ListItem>
+                  {allSpaces.map((space, index) => (
+                    <ListItem key={space}>
+                      <IconButton
+                        aria-label={`Delete ${space}`}
+                        icon={<DeleteIcon />}
+                        onClick={() => handleSpaceDelete(index)}
+                        mr={4}
+                      />
+                      {space}
+                    </ListItem>
                   ))}
                 </UnorderedList>
               </VStack>
 
-              <VStack as="form" onSubmit={handleSubmit}>
+              <VStack as="form" onSubmit={handleGameStart}>
                 <FormControl id="board-game-name" isRequired>
                   <FormLabel>Enter Board Game Name</FormLabel>
                   <Input name="board_name" />
@@ -193,15 +254,15 @@ function App() {
                 </FormControl>
                 <FormControl id="top-right-name" isRequired>
                   <FormLabel>Top-Right Corner Name</FormLabel>
-                  <Input name="top_right" />
+                  <Input name="top_right" defaultValue="Jail" />
                 </FormControl>
                 <FormControl id="bottom-right-name" isRequired>
                   <FormLabel>Bottom-Right Corner Name</FormLabel>
-                  <Input name="bottom_right" />
+                  <Input name="bottom_right" defaultValue="Free Parking" />
                 </FormControl>
                 <FormControl id="bottom-left-name" isRequired>
                   <FormLabel>Bottom-Left Corner Name</FormLabel>
-                  <Input name="bottom_left" />
+                  <Input name="bottom_left" defaultValue="Go to Jail" />
                 </FormControl>
                 <Button type="submit" width="full" colorScheme="orange">
                   Start Board
@@ -210,7 +271,7 @@ function App() {
             </SimpleGrid>
           </Container>
         )}
-      </Box>
+      </Flex>
     </ChakraProvider>
   );
 }
